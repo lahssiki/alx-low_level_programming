f8982d1daac37ef23b75eae365ffdddc11b23706
lahssiki <anaslahssiki@gmail.com>
2023-03-16 20:54:00 +0100

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *  * main - prints a given string
- *   *
- *    * Return: 0 Always
- *     */
+ * main - prints a given string
+ *
+ * Return: 0 Always
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 }
