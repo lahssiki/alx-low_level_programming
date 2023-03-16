43422cf1288941acb8f614b2e3edf96f569a3dd0
lahssiki <anaslahssiki@gmail.com>
2023-03-16 21:05:25 +0100

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given string
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+}
