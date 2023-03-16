004079b56867b610129367b3355696cee405642d
lahssiki <anaslahssiki@gmail.com>
2023-03-16 21:40:22 +0100

    Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - prints a given string
- *
- * Return: 0 Always
+ * main - entry point
+ * Return: 1 (Success)
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19);
-
+		write(1, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n", 59);
 		return (1);
 }
