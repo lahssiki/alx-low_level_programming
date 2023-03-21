c84b46ebe774336dafdcb68bd9cd8ccceaed99a3
lahssiki <anaslahssiki@gmail.com>
2023-03-21 16:34:05 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	char str[] = "_putchar\n";
-	int i = 0;
-
-	while (str[i] != '\0')
-		{
-		char c = str[i];
-
-		_putchar(c);
-		i++;
-		}
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
