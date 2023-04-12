5db1c5872840f63a06bb88364ce41080f7186f7a
Anas Lahssiki <anaslahssiki@gmail.com>
2023-04-12 01:29:54 +0000

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - Returns a pointer to a 2-dimensional array 
+ * alloc_grid - Returns a pointer to a 2-dimensional array.
  * @width: The width of the 2-dimensional array.
  * @height: The height of the 2-dimensional array.
  *
