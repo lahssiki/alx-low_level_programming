2ee9d88ce926f4e86926aacd6bc4522e41f0aa2b
Anas Lahssiki <anaslahssiki@gmail.com>
2023-04-18 00:27:35 +0000

    modi task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,8 +2,9 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /*
- * File: 3-function_like_macro.h.
- * Desc: Header file that defines a function-like macro ABS(x)
+ * File: 3-function_like_macro.h
+ *
+ * Desc: Header file that defines a function-like macro ABS(x).
  *       that computes the absolute value of a number x.
  */
 
