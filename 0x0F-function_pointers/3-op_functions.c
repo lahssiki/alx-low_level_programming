bf9ffea400d2a237333d33116cbe2e0208d4d7db
Anas Lahssiki <anaslahssiki@gmail.com>
2023-04-20 00:51:52 +0000

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub 
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div 
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
