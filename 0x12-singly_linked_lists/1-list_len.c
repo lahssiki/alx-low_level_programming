e31787dbcb6d8f8bf0d427672332c14a43a96d3f
lahssiki <anaslahssiki@gmail.com>
2023-04-28 00:21:39 +0100

    Task 2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,13 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t eleme = 0;
+	size_t elements = 0;
 
 	while (h)
 	{
-		eleme++;
+		elements++;
 		h = h->next;
 	}
 
-	return (eleme);
+	return (elements);
 }
